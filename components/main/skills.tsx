f77b30d278b3e38ce3f97a88eca9ab0e898223c5
Artreeus <142108262+Artreeus@users.noreply.github.com>
2024-07-14 02:55:10 +0600

    project fix

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -31,18 +31,7 @@ export const Skills = () => {
         ))}
       </div>
 
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {FRONTEND_SKILL.map((skill, i) => (
-          <SkillDataProvider
-            key={skill.skill_name}
-            src={skill.image}
-            name={skill.skill_name}
-            width={skill.width}
-            height={skill.height}
-            index={i}
-          />
-        ))}
-      </div>
+      
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {BACKEND_SKILL.map((skill, i) => (
           <SkillDataProvider
